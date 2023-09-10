9259c694fd4f4b1e1d45058ba480ccc459d2ad12
sandy11JsDs <jorgesandy965@gmail.com>
2023-09-10 09:45:16 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appweboperaciones/AppWebOperacionesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appweboperaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebOperacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebOperacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appweboperaciones/AppWebOperacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appweboperaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebOperacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
